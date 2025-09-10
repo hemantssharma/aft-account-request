6c5d3815cd11908ef03b436046d142a9827a6a64
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-11 00:16:33 +0530

    Update dev-team1.tf

--- terraform/dev-team1.tf
+++ terraform/dev-team1.tf
@@ -1,11 +1,11 @@
 module "dev_team1" {
-  source  = "aws-ia/control_tower_account_factory_request/aws"
-  version = "~> 1.0" # or the version you want to pin
+  source  = "aws-ia/account-request/aws"
+  version = "~> 1.2" # pin to latest compatible release
 
   control_tower_parameters = {
     AccountEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
     AccountName               = "devaccount-3"
-    ManagedOrganizationalUnit = "Sandbox"
+    ManagedOrganizationalUnit = "Sandbox"   # must exist in Control Tower
     SSOUserEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
     SSOUserFirstName          = "Dev"
     SSOUserLastName           = "Account-3"
