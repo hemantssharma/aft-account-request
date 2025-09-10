c85be16857789202e7c9e1374894ab951f1bce66
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-11 00:20:53 +0530

    Update dev-team1.tf

--- terraform/dev-team1.tf
+++ terraform/dev-team1.tf
@@ -1,11 +1,11 @@
 module "dev_team1" {
   source  = "aws-ia/account-request/aws"
-  version = "~> 1.2" # pin to latest compatible release
+  version = "1.3.3" # latest stable at time of writing
 
   control_tower_parameters = {
     AccountEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
     AccountName               = "devaccount-3"
-    ManagedOrganizationalUnit = "Sandbox"   # must exist in Control Tower
+    ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
     SSOUserFirstName          = "Dev"
     SSOUserLastName           = "Account-3"
