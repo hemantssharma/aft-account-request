9ffeb02f734642ee81de9ecc3dd1983b471bbccb
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-11 00:12:56 +0530

    Update dev-team1.tf

--- terraform/dev-team1.tf
+++ terraform/dev-team1.tf
@@ -1,19 +1,17 @@
-locals {
-  account_requests = {
-    dev-team1 = {
-      control_tower_parameters = {
-        AccountEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
-        AccountName               = "devaccount-3"
-        ManagedOrganizationalUnit = "Sandbox"
-        SSOUserEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
-        SSOUserFirstName          = "Dev"
-        SSOUserLastName           = "Account-3"
-      }
- 
-      account_tags = {
-        owner = "Dev"
-        env   = "sandbox"
-      }
-    }
+module "dev_team1" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory_request"
+
+  control_tower_parameters = {
+    AccountEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
+    AccountName               = "devaccount-3"
+    ManagedOrganizationalUnit = "Sandbox"   # must exist in Control Tower
+    SSOUserEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
+    SSOUserFirstName          = "Dev"
+    SSOUserLastName           = "Account-3"
+  }
+
+  account_tags = {
+    owner = "Dev"
+    env   = "sandbox"
   }
 }
