4538cefa177c8e48f4d3c59e0b8e99e8b78c0b10
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-11 00:43:21 +0530

    Update dev-team1.tf

--- terraform/dev-team1.tf
+++ terraform/dev-team1.tf
@@ -1,5 +1,5 @@
 module "dev_team1" {
-  source  = "git::https://github.com/aws-ia/terraform-aws-control_tower_account_factory_request.git?ref=v1.3.3"
+  source = "git::https://oauth2:${var.github_token}@github.com/aws-ia/terraform-aws-control_tower_account_factory_request.git?ref=v1.3.3"
 
   control_tower_parameters = {
     AccountEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
