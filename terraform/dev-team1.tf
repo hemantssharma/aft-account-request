43d7579cc241e6c6c7d7dbef41cc883253e23541
hemantssharma <113016398+hemantssharma@users.noreply.github.com>
2025-09-11 13:29:36 +0530

    Update dev-team1.tf

--- terraform/dev-team1.tf
+++ terraform/dev-team1.tf
@@ -1,14 +1,15 @@
 module "dev_team1" {
-  source  = "aws-ia/control-tower-account-factory-request/aws"
-  version = "1.3.3"
+  source = "modules/account-request"
+
   control_tower_parameters = {
-    AccountEmail              = "mr.hemantksharma+devaccount-1@gmail.com"
-    AccountName               = "devaccount-1"
+    AccountEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
+    AccountName               = "devaccount-3"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "mr.hemantksharma+devaccount-1@gmail.com"
+    SSOUserEmail              = "mr.hemantksharma+devaccount-3@gmail.com"
     SSOUserFirstName          = "Dev"
-    SSOUserLastName           = "Account-1"
+    SSOUserLastName           = "Account-3"
   }
+
   account_tags = {
     owner = "Dev"
     env   = "sandbox"
